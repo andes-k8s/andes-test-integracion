9b43b2aecc564a173cde6f1453b1838cabc1a166
nahirsaddiRTO <nahirsaddi@gmail.com>
2019-02-13 08:36:07 -0300

    Arreglo de casos +  caso 9, 3, 10, 11

--- cypress/integration/internacion/egreso.spec.js
+++ cypress/integration/internacion/egreso.spec.js
@@ -17,27 +17,22 @@ context('Aliasing', () => {
     })
 
 
-    // it('ingreso camino feliz', () => {
-
+    // it('Egreso datos minimos', () => {
     //     cy.get('.ocupada').first().click();
-    //     cy.get('.mdi-close-box').first().click({ force: true });
+    //     cy.get('.mdi-format-list-bulleted').first().click({ force: true });
     //     cy.get('#mat-radio-3').children().children('.mat-radio-container').click({ force: true });
     //     cy.get('.mdi-arrow-down-drop-circle-outline').click({ force: true });
     //     cy.get('plex-select[label="Tipo de egreso"] input').type('alta')
     //     cy.get('plex-select[label="Tipo de egreso"]').children().children().children('.selectize-input').click({ force: true }).get('.option[data-value="Alta médica"]').click({ force: true })
-    //     cy.get('plex-select[label="Diagnostico Principal al egreso"] input').type('acne tropical')
-    //     cy.wait(2000)
-    //     cy.get('.option[data-value="59bbf1ee53916746547cc10b"]').click({ force: true })
     //     cy.get('plex-button[label="Guardar"]').click();
-    //     cy.get('plex-button[label="Validar Internación"]').click();
-    //     cy.get('button').contains('CONFIRMAR').click()         
+    //     cy.contains('Prestacion guardada');
+    //  cy.log('Termino bien')
     // })
 
 
     // it('evento obstétrico', () => {
-
     //     cy.get('.ocupada').first().click();
-    //     cy.get('.mdi-close-box').first().click({ force: true });
+    //     cy.get('.mdi-format-list-bulleted').first().click({ force: true });
     //     cy.get('#mat-radio-3').children().children('.mat-radio-container').click({ force: true });
     //     cy.get('.mdi-arrow-down-drop-circle-outline').click({ force: true });
     //     cy.get('plex-select[label="Tipo de egreso"] input').type('alta')
@@ -49,22 +44,18 @@ context('Aliasing', () => {
     //     cy.get('plex-int[name="edadGestacional"] input').first().type(85);
     //     cy.get('plex-int[label="Paridad"] input').first().type(85);
     //     cy.get('plex-int[label="Peso al nacer"] input').first().type(85);
-
-
     //     cy.get('mat-radio-button[ng-reflect-value="Simple"] input').first().click({ force: true })
-
     //     cy.get('mat-radio-button[ng-reflect-value="Nac. Vivo"] input').first().click({ force: true })
     //     cy.get('mat-radio-button[ng-reflect-value="Vaginal"] input').first().click({ force: true })
     //     cy.get('mat-radio-button[ng-reflect-value="Masculino"] input').first().click({ force: true })
     //     cy.get('plex-button[label="Guardar"]').click();
-    //     cy.get('plex-button[label="Validar Internación"]').click();
-    //     cy.get('button').contains('CONFIRMAR').click()
+    //     cy.contains('Prestacion guardada');
+    //  cy.log('Termino bien')
     // })
 
     // it('evento externo', () => {
-
     //     cy.get('.ocupada').first().click();
-    //     cy.get('.mdi-close-box').first().click({ force: true });
+    //     cy.get('.mdi-format-list-bulleted').first().click({ force: true });
     //     cy.get('#mat-radio-3').children().children('.mat-radio-container').click({ force: true });
     //     cy.get('.mdi-arrow-down-drop-circle-outline').click({ force: true });
     //     cy.get('plex-select[label="Tipo de egreso"] input').type('alta')
@@ -74,81 +65,122 @@ context('Aliasing', () => {
     //     cy.get('.option[data-value="59bbf1ef53916746547ccc04"]').click({ force: true })
     //     cy.get('plex-select[label="Producido por:"] input').type('Accidente')
     //     cy.get('.option[ data-value="Accidente"]').click({ force: true })
-
-
     //     cy.get('plex-select[label="Lugar donde ocurrio"] input').type('otro')
     //     cy.get('.option[ data-value="otro"]').click({ force: true })
-
     //     cy.get('plex-select[label="Como se produjo"] input').type('w21.4')
     //     cy.get('.option[data-value="59bbf1f053916746547cd4de"]').click({ force: true })
-    //        cy.get('plex-button[label="Guardar"]').click();
-    //     cy.get('plex-button[label="Validar Internación"]').click();
-    //     cy.get('button').contains('CONFIRMAR').click()
+    //     cy.get('plex-button[label="Guardar"]').click();
+    //     cy.contains('Prestacion guardada');
+    //  cy.log('Termino bien')
     // })
 
     // it('egreso por traslado', () => {
-
-    // cy.get('.ocupada').first().click();
-    // cy.get('.mdi-close-box').first().click({ force: true });
-    // cy.get('#mat-radio-3').children().children('.mat-radio-container').click({ force: true });
-    // cy.get('.mdi-arrow-down-drop-circle-outline').click({ force: true });
-    // cy.get('plex-select[label="Tipo de egreso"] input').type('Traslado')
-    // cy.get('plex-select[label="Tipo de egreso"]').children().children().children('.selectize-input').click({ force: true }).get('.option[data-value="Traslado"]').click({ force: true })
-
-
-    // cy.get('plex-select[label="Unidad organizativa de destino"] input').type('heller')
-    // cy.get('.option[data-value="57fcf038326e73143fb48dac"]').click({ force: true })
-    // cy.get('plex-select[label="Diagnostico Principal al egreso"] input').type('G47.3')
-    // cy.get('.option[data-value="59bbf1ed53916746547cb9e9"]').click({ force: true })
-    //          cy.get('plex-button[label="Guardar"]').click();
-    // cy.get('plex-button[label="Validar Internación"]').click();
-    // cy.get('button').contains('CONFIRMAR').click()
-
-
-
+    //     cy.get('.ocupada').first().click();
+    //     cy.get('.mdi-format-list-bulleted').first().click({ force: true });
+    //     cy.get('#mat-radio-3').children().children('.mat-radio-container').click({ force: true });
+    //     cy.get('.mdi-arrow-down-drop-circle-outline').click({ force: true });
+    //     cy.get('plex-select[label="Tipo de egreso"] input').type('Traslado')
+    //     cy.get('plex-select[label="Tipo de egreso"]').children().children().children('.selectize-input').click({ force: true }).get('.option[data-value="Traslado"]').click({ force: true })
+    //     cy.get('plex-select[label="Unidad organizativa de destino"] input').type('heller')
+    //     cy.get('.option[data-value="57fcf038326e73143fb48dac"]').click({ force: true })
+    //     cy.get('plex-select[label="Diagnostico Principal al egreso"] input').type('G47.3')
+    //     cy.get('.option[data-value="59bbf1ed53916746547cb9e9"]').click({ force: true })
+    //     cy.get('plex-button[label="Guardar"]').click();
+    //     cy.contains('Prestacion guardada');
+    //  cy.log('Termino bien')
     // })
-    it('egreso por traslado', () => {
-        cy.get('.card-container').then(($body) => {
-            if ($body.hasClass('ocupada')) {
-                console.log("aca si")
-                cy.get('.ocupada').first().click();
-                cy.get('.mdi-close-box').first().click({ force: true });
-                cy.get('#mat-radio-3').children().children('.mat-radio-container').click({ force: true });
-                cy.get('.mdi-arrow-down-drop-circle-outline').click({ force: true });
-                cy.get('plex-select[label="Tipo de egreso"] input').type('alta')
-                cy.get('plex-select[label="Tipo de egreso"]').children().children().children('.selectize-input').click({ force: true }).get('.option[data-value="Alta médica"]').click({ force: true })
-                cy.get('plex-select[label="Diagnostico Principal al egreso"] input').type('acne tropical')
-                cy.wait(2000)
-                cy.get('.option[data-value="59bbf1ee53916746547cc10b"]').click({ force: true })
-                cy.get('plex-button[label="Guardar"]').click();
-                cy.get('plex-button[label="Validar Internación"]').click();
-                cy.get('button').contains('CONFIRMAR').click()
-                
-            }
-            if ($body.hasClass('desocupada')) {
-                cy.get('.desocupada').first().click();
-                cy.get('.mdi-spray').first().click({ force: true });
-
-            }
-            if ($body.hasClass('disponible')) {
-                cy.get('.disponible').first().click();
-            }
-        })
 
 
-
-
-    })
+    // it('egreso por traslado', () => {
+    //     cy.get('.card-container').then(($body) => {
+    //         if ($body.hasClass('ocupada')) {
+    //             cy.get('.ocupada').first().click();
+    //             cy.get('.mdi-format-list-bulleted').first().click({ force: true });
+    //             cy.get('#mat-radio-3').children().children('.mat-radio-container').click({ force: true });
+    //             cy.get('.mdi-arrow-down-drop-circle-outline').click({ force: true });
+    //             cy.get('plex-select[label="Tipo de egreso"] input').type('alta')
+    //             cy.get('plex-select[label="Tipo de egreso"]').children().children().children('.selectize-input').click({ force: true }).get('.option[data-value="Alta médica"]').click({ force: true })
+    //             cy.get('plex-select[label="Diagnostico Principal al egreso"] input').type('acne tropical')
+    //             cy.wait(2000)
+    //             cy.get('.option[data-value="59bbf1ee53916746547cc10b"]').click({ force: true })
+    //             cy.get('plex-button[label="Guardar"]').click();
+    //             cy.contains('Prestacion guardada');
+    //             cy.log('Termino bien')
+    //         }
+    //     })
+    // })
 
 
 
     // it('fecha de egreso anterior a la fecha de ingreso', () => {
+    //     cy.get('.card-container').then(($body) => {
+    //         if ($body.hasClass('ocupada')) {
+    //             cy.get('.ocupada').first().click();
+    //             cy.get('.mdi-format-list-bulleted').first().click({ force: true });
+    //             cy.get('#mat-radio-3').children().children('.mat-radio-container').click({ force: true });
+    //             cy.get('.mdi-arrow-down-drop-circle-outline').click({ force: true });
+    //             cy.get('plex-dateTime[label="Fecha"] input').clear()
+    //             cy.get('plex-dateTime[label="Fecha"] input').first().type('15012019').should('have.value', '15012019');
+    //             cy.contains('ERROR: La fecha de egreso no puede ser inferior');
+    //             cy.log('Termino bien')
+    //         }
+    //     })
+    // })
+
+    // it('fecha de egreso anterior a la fecha del ultimo movimiento', () => {
+    //     cy.get('.card-container').then(($body) => {
+    //         if ($body.hasClass('ocupada')) {
+    //             cy.get('.ocupada').first().click();
+    //             cy.get('.mdi-format-list-bulleted').first().click({ force: true });
+    //             cy.get('#mat-radio-3').children().children('.mat-radio-container').click({ force: true });
+    //             cy.get('.mdi-arrow-down-drop-circle-outline').click({ force: true });
+    //             cy.get('plex-dateTime[label="Fecha"] input').clear()
+    //             cy.get('plex-dateTime[label="Fecha"] input').first().type('11022019').should('have.value', '11022019');
+    //             cy.contains('ERROR: La fecha de egreso no puede ser inferior a ');
+    //             cy.log('Termino bien')
+    //         }
+    //     })
+    // })
+
+    // it('egreso datos completos', () => {
     //     cy.get('.ocupada').first().click();
-    //     cy.get('.mdi-close-box').first().click({ force: true });
+    //     cy.get('.mdi-format-list-bulleted').first().click({ force: true });
     //     cy.get('#mat-radio-3').children().children('.mat-radio-container').click({ force: true });
     //     cy.get('.mdi-arrow-down-drop-circle-outline').click({ force: true });
-    //     cy.get('plex-dateTime[name="fecha-hora"] input').clear()
-    //     cy.get('plex-dateTime[name="fecha-hora"] input').first().type('15012019').should('have.value', '15012019');
-    // })
+    //     cy.get('plex-select[label="Tipo de egreso"] input').type('Trasl')
+    //     cy.wait(2000)
+    //     cy.get('.option[data-value="Traslado"]').click();
+    //     cy.get('plex-select[label="Unidad organizativa de destino"] input').type('heller')
+    //     cy.get('.option[data-value="57fcf038326e73143fb48dac"]').click({ force: true })
+    //     cy.get('plex-select[label="Diagnostico Principal al egreso"] input').type('T98.2')
+    //     cy.wait(2000)
+    //     cy.get('.option[data-value="59bbf1f053916746547cd0fc"]').click({ force: true })
+    //     cy.get('plex-select[label="Otros Diagnósticos"] input').type('O84.9')
+    //     cy.wait(2000)
+    //     cy.get('.option[data-value="59bbf1ee53916746547cc6ae"]').click({ force: true })
+    //     cy.get('plex-select[label="Otras circunstancias"] input').type('G03.1')
+    //     cy.wait(2000)
+    //     cy.get('.option[data-value="59bbf1ed53916746547cb960"]').click({ force: true })
+    //     cy.get('plex-int[label="Otros días de estada"] input').type(6)
+    //     cy.get('plex-int[label="Días de permiso de salida"] input').type(4)
+    //     cy.get('plex-select[label="Producido por:"] input').type('agresion')
+    //     cy.get('.option[ data-value="agresion"]').click({ force: true })
+    //     cy.get('plex-select[label="Lugar donde ocurrio"] input').type('Vía pública')
+    //     cy.get('.option[ data-value="viaPublico"]').click({ force: true })
+    //     cy.get('plex-select[label="Como se produjo"] input').type('Y07.9')
+    //     cy.get('.option[data-value="59bbf1f053916746547cdb88"]').click({ force: true })
+    //     cy.get('plex-select[label="Procedimientos"] input').type('Inserción de dispositivos')
+    //     cy.get('.option[data-value="5a944889968364d0143dd0ef"]').click({ force: true })
+    //     cy.get('plex-dateTime[label="Fecha"] input').eq(1).type('10022019');
+    //     cy.get('plex-button[label="Agregar procedimiento"]').click()
+    //     cy.get('plex-select[label="Procedimientos"] input').eq(1).type('EXAMEN')
+    //     cy.get('.option[data-value="5a944889968364d0143dd0ed"]').eq(1).click({ force: true })
+    //     cy.get('plex-dateTime[label="Fecha"] input').eq(2).type('090022019');
+    //     cy.get('plex-button[label="Agregar procedimiento"]').click();
+    //     cy.get('div[class="col-2 p-1 float-right"]').eq(2).click();
+    //     cy.get('plex-button[label="Guardar"]').click();
+    //     cy.contains('Prestacion guardada');
+    //  cy.log('Termino bien')
+    // });
 
 })
--- cypress/integration/internacion/ingreso.spec.js
+++ cypress/integration/internacion/ingreso.spec.js
@@ -3,7 +3,7 @@
 context('Aliasing', () => {
     let token
     before(() => {
-        cy.login('38906735', 'asd').then(t => {
+        cy.login('37781560', 'asd').then(t => {
             token = t;
         })
     })
@@ -17,22 +17,91 @@ context('Aliasing', () => {
     })
 
 
-    it('ingreso camino feliz', () => {
+    // it('ingreso camino feliz', () => {
 
+    //     cy.get('.disponible').first().click();
+    //     cy.get('.mdi-account-plus').first().click({ force: true });
+    //     cy.get('plex-text[name="buscador"] input').first().type('38906735');
+    //     cy.get('table tr').contains('38906735').first().click()
+    //     cy.get('plex-select[label="Origen hospitalización"] input').type('Otro')
+    //     cy.wait(2000)
+    //     cy.get('.option[data-value="Otro"]').click()
+    //     cy.get('plex-button').contains('INICIAR').click({ force: true });
+    // //     cy.get('plex-button[label="Guardar"]').click();
+
+
+
+
+
+    // })
+
+    // it('ingreso workflow simple datos completos', () => {
+    //     cy.get('.disponible').first().click();
+    //     cy.get('.mdi-account-plus').first().click({ force: true });
+    //     cy.get('plex-text[name="buscador"] input').first().type('25715826');
+    //     cy.get('table tr').contains('25715826').first().click();
+    //     cy.wait(4000)
+    //     cy.get('plex-button').contains('Cambiar Paciente').click();
+    //     cy.get('plex-text[name="buscador"] input').first().type('16314916');
+    //     cy.get('table tr').contains('16314916').first().click();
+    //     cy.get('plex-select[label="Cobertura"] input').type('Ninguno');
+    //     cy.wait(1000)
+    //     cy.get('.option[data-value="Ninguno"]').click();
+    //     cy.get('plex-button').contains('Nueva Carpeta').click({ force: true });
+    //     cy.get('plex-button[label="Cancelar"]').click();
+    //     cy.get('plex-select[label="Origen hospitalización"] input').type('Consultorio externo')
+    //     cy.wait(2000)
+    //     cy.get('.option[data-value="Consultorio externo"]').click()
+    //     cy.get('plex-select[label="Organización origen"] input').type('castro')
+    //     cy.wait(2000)
+    //     cy.get('.option[data-value="57e9670e52df311059bc8964"]').click({ force: true })
+    //     cy.get('plex-text[name="motivo"] input').first().type('apendicitis');
+    //     cy.get('plex-select[label=" Situación laboral "] input').type('Trabaja o está de licencia')
+    //     cy.wait(2000)
+    //     cy.get('.option[data-value="Trabaja o está de licencia"]').click();
+    //     cy.get('plex-select[label="Ocupación habitual "] input').type('Actor')
+    //     cy.wait(2000)
+    //     cy.get('.option[data-value="Actor"]').click()
+    //     cy.get('plex-select[label="Nivel instrucción"] input').type('Secundario incompleto')
+    //     cy.wait(2000)
+    //     cy.get('.option[data-value="Secundario incompleto"]').click()
+    //     cy.get('plex-button').contains('INICIAR').click({ force: true });
+    //     cy.get('plex-button[label="Guardar"]').click();
+    // })
+
+    // it('Ingreso en una cama ocupada', () => {
+    //     cy.get('.disponible').first().click();
+    //     cy.get('.mdi-account-plus').first().click({ force: true });
+    //     cy.get('plex-text[name="buscador"] input').first().type('30178762');
+    //     cy.get('table tr').contains('30178762').first().click();
+    //     cy.wait(4000)
+    //     cy.get('plex-dateTime[label="Fecha Ingreso"] input').clear();
+    //     cy.get('plex-dateTime[label="Hora Ingreso"] input').clear();
+    //     // Es una cama que está ocupada el 11/02 desde las 15:10 a las 15:30, por lo que a las 15:12 no me debera dejar ingresar.
+    //     cy.get('plex-dateTime[label="Fecha Ingreso"] input').first().type('11/02/2019');
+    //     cy.get('plex-dateTime[label="Hora Ingreso"] input').first().type('15:12');
+    //     cy.get('plex-select[label="Origen hospitalización"] input').type('Otro')
+    //     cy.get('.option[data-value="Otro"]').click()
+    //     cy.get('plex-button').contains('INICIAR').click({ force: true });
+    //     cy.contains('La cama seleccionada no está disponible en la fecha ingresada. Por favor controle la fecha y hora de ingreso.');
+    //     cy.log('Termino bien');
+    // })
+
+    it('ingreso por traslado', () => {
         cy.get('.disponible').first().click();
         cy.get('.mdi-account-plus').first().click({ force: true });
-        cy.get('plex-text[name="buscador"] input').first().type('38906735');
-        cy.get('table tr').contains('38906735').first().click()
-        cy.get('plex-select[label="Origen hospitalización"] input').type('Otro')
+        cy.get('plex-text[name="buscador"] input').first().type('30178762');
+        cy.get('table tr').contains('30178762').first().click();
+        cy.wait(4000)
+        cy.get('plex-select[label="Origen hospitalización"] input').type('Trasl')
         cy.wait(2000)
-        cy.get('.option[data-value="Otro"]').click()
+        cy.get('.option[data-value="Traslado"]').click()
+        cy.get('plex-select[label="Organización origen"] input').type('heller')
+        cy.wait(2000)
+        cy.get('.option[data-value="57fcf038326e73143fb48dac"]').click({ force: true })
         cy.get('plex-button').contains('INICIAR').click({ force: true });
-    //     cy.get('plex-button[label="Guardar"]').click();
-
-        
-
-        
-
+        cy.log('Termino bien');
     })
+
 
 })
